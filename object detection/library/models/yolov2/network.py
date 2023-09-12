e2a9fb4b293f6afe4a8ee47957d0c359fa20bd6d
gitE0Z9 <acrocanthosaurus627@gmail.com>
2023-09-12 23:31:11 +0800

    style

--- object detection/library/models/yolov2/network.py
+++ object detection/library/models/yolov2/network.py
@@ -51,6 +51,27 @@ class BottleNeck(nn.Module):
         return tmp
 
 
+class ReorgLayer(nn.Module):
+    def __init__(self, stride: int):
+        """Stack patch
+
+        Args:
+            stride (int): stride
+        """
+        super(ReorgLayer, self).__init__()
+        self.stride = stride
+
+    def forward(self, x: Tensor) -> Tensor:
+        b, c, h, w = x.shape
+        x = x.reshape(
+            b,
+            c * self.stride * self.stride,
+            h // self.stride,
+            w // self.stride,
+        )
+        return x
+
+
 class Darknet19(nn.Module):
     def __init__(self):
         """Darknet19"""
@@ -94,24 +115,3 @@ class Darknet19(nn.Module):
         tmp = tmp.mean(dim=(2, 3))
 
         return tmp
-
-
-class ReorgLayer(nn.Module):
-    def __init__(self, stride: int):
-        """Stack patch
-
-        Args:
-            stride (int): stride
-        """
-        super(ReorgLayer, self).__init__()
-        self.stride = stride
-
-    def forward(self, x: Tensor) -> Tensor:
-        b, c, h, w = x.shape
-        x = x.reshape(
-            b,
-            c * self.stride * self.stride,
-            h // self.stride,
-            w // self.stride,
-        )
-        return x
