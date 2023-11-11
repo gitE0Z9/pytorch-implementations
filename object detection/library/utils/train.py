a52f8b83cf2994e890f32f4e1b402bd5bc5b19c9
gitE0Z9 <acrocanthosaurus627@gmail.com>
2023-11-12 03:08:18 +0800

    fix union area in batch iou

--- object detection/library/utils/train.py
+++ object detection/library/utils/train.py
@@ -33,15 +33,15 @@ def generate_grid_train(grid_x: int, grid_y: int, center: bool = False) -> torch
 def xywh_to_xyxy(coord: torch.Tensor) -> torch.Tensor:
     """convert xywh to xyxy"""
     b, na, _, h, w = coord.shape
-    d = "cuda:0" if coord.is_cuda else "cpu"
-    grid_for_train = generate_grid_train(w, h).repeat(b, na, 1, 1, 1).to(d)
+    device = "cuda:0" if coord.is_cuda else "cpu"
+    grid_for_train = generate_grid_train(w, h).repeat(b, na, 1, 1, 1).to(device)
     xy = coord[:, :, 0:2, :, :] + grid_for_train  # B, na, 2, 7, 7
     wh = coord[:, :, 2:4, :, :]  # B, na, 2, 7, 7
 
     return torch.cat([xy - wh / 2, xy + wh / 2], dim=2)  # B, na, 4, 7, 7
 
 
-def IOU(pred_cbox: torch.Tensor, gt_cbox: torch.Tensor) -> torch.Tensor:
+def IOU(pred_box: torch.Tensor, gt_box: torch.Tensor) -> torch.Tensor:
     """
     input: x_offset y_offset wh format
     output: iou for each batch and grid cell
@@ -51,23 +51,23 @@ def IOU(pred_cbox: torch.Tensor, gt_cbox: torch.Tensor) -> torch.Tensor:
     epsilon = 1e-10
 
     # coord_conversion
-    pred_box = xywh_to_xyxy(pred_cbox)  # B, 5, 4, 13, 13
-    gt_box = xywh_to_xyxy(gt_cbox)  # B, 1, 4, 13, 13
+    converted_pred_box = xywh_to_xyxy(pred_box)  # B, 5, 4, 13, 13
+    converted_gt_box = xywh_to_xyxy(gt_box)  # B, 1, 4, 13, 13
 
     # find intersection
-    x1 = torch.max(pred_box[:, :, 0:1, :, :], gt_box[:, :, 0:1, :, :])
-    y1 = torch.max(pred_box[:, :, 1:2, :, :], gt_box[:, :, 1:2, :, :])
-    x2 = torch.min(pred_box[:, :, 2:3, :, :], gt_box[:, :, 2:3, :, :])
-    y2 = torch.min(pred_box[:, :, 3:4, :, :], gt_box[:, :, 3:4, :, :])
+    x1 = torch.max(converted_pred_box[:, :, 0:1, :, :], converted_gt_box[:, :, 0:1, :, :])
+    y1 = torch.max(converted_pred_box[:, :, 1:2, :, :], converted_gt_box[:, :, 1:2, :, :])
+    x2 = torch.min(converted_pred_box[:, :, 2:3, :, :], converted_gt_box[:, :, 2:3, :, :])
+    y2 = torch.min(converted_pred_box[:, :, 3:4, :, :], converted_gt_box[:, :, 3:4, :, :])
 
-    intersection = (x2 - x1).clamp(min=0) * (y2 - y1).clamp(min=0)  # N, 5, 1, 7, 7
+    intersection = (x2 - x1).clamp(min=0, max=1) * (y2 - y1).clamp(min=0, max=1)  # N, 5, 1, 7, 7
     pred_area = pred_box[:, :, 2:3, :, :] * pred_box[:, :, 3:4, :, :]  # N, 5, 1, 7, 7
     gt_area = gt_box[:, :, 2:3, :, :] * gt_box[:, :, 3:4, :, :]  # N, 1, 1, 7, 7
     total_area = pred_area + gt_area - intersection  # N, 5, 1, 7, 7
     total_area = total_area.clamp(min=0, max=1)
 
-    assert intersection.ge(0).all(), "intersection should be more than 0"
-    assert total_area.ge(0).all(), "total area should be more than 0"
+    assert intersection.ge(0).all(), "intersection should be no less than 0"
+    assert total_area.ge(0).all(), "total area should be no less than 0"
 
     # compute iou
     intersection[intersection.gt(0)] = (
