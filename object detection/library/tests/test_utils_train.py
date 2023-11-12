702ee5f22b00bea186a2f79e9c860d515e71e375
gitE0Z9 <acrocanthosaurus627@gmail.com>
2023-11-12 10:55:10 +0800

    add practical test to utils train

--- object detection/library/tests/test_utils_train.py
+++ object detection/library/tests/test_utils_train.py
@@ -1,5 +1,5 @@
 import torch
-from torch.testing import assert_equal
+from torch.testing import assert_close
 
 from utils.train import (
     generate_grid_train,
@@ -12,19 +12,47 @@ from utils.train import (
 
 class TestUtilsTrain:
     def test_generate_grid_train_shape(self):
-        """not useful test"""
         grid = generate_grid_train(13, 13)
-        assert_equal(grid.shape, torch.Size([1, 1, 2, 13, 13]))
+        
+        assert_close(grid.shape, torch.Size([1, 1, 2, 13, 13]))
 
     def test_xywh_to_xyxy_shape(self):
-        """not useful test"""
         testx = torch.rand((2, 5, 4, 13, 13))
+        
         convert_x = xywh_to_xyxy(testx)
-        assert_equal(testx.shape, convert_x.shape)
+        assert_close(testx.shape, convert_x.shape)
+        
+    def test_xywh_to_xyxy_overlap(self):
+        x = torch.Tensor([[.25, .25, .5, .5, 1], [.25, .25, .5, .5, .9]]).view(2, 1, 5, 1, 1)
+        
+        convert_x = xywh_to_xyxy(x[:, :4])
+        should_x = torch.Tensor([[0, 0, .5, .5], [0, 0, .5, .5]]).view(2, 1, 4, 1, 1)
+        assert_close(convert_x, should_x)
+        
+    def test_xywh_to_xyxy_non_overlap(self):
+        x = torch.Tensor([[.25, .25, .5, .5, 1], [.75, .75, .5, .5, .9]]).view(2, 1, 5, 1, 1)
+        
+        convert_x = xywh_to_xyxy(x[:, :4])
+        should_x = torch.Tensor([[0, 0, .5, .5], [.5, .5, 1, 1]]).view(2, 1, 4, 1, 1)
+        assert_close(convert_x, should_x)
 
     def test_iou_shape(self):
-        """not useful test"""
         testx = torch.rand((2, 5, 4, 13, 13))
         testy = torch.rand((2, 1, 4, 13, 13))
+        
         ious = IOU(testx, testy)
-        assert_equal(ious.shape, torch.Size([2, 5, 1, 13, 13]))
+        assert_close(ious.shape, torch.Size([2, 5, 1, 13, 13]))
+        
+    def test_iou_overlap(self):
+        x = torch.Tensor([[.25, .25, .5, .5, 1]]).view(1, 1, 5, 1, 1)
+        y = torch.Tensor([[.25, .25, .5, .5, .9]]).view(1, 1, 5, 1, 1)
+        
+        ious = IOU(x, y)
+        assert_close(ious, torch.Tensor([1]).view(1, 1, 1, 1, 1))
+        
+    def test_iou_non_overlap(self):
+        x = torch.Tensor([[.25, .25, .5, .5, 1]]).view(1, 1, 5, 1, 1)
+        y = torch.Tensor([[.75, .75, .5, .5, .9]]).view(1, 1, 5, 1, 1)
+        
+        ious = IOU(x, y)
+        assert_close(ious, torch.Tensor([0]).view(1, 1, 1, 1, 1))
