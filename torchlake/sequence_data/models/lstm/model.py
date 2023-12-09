b13d1b18647567e2d1fbe963c30cb0e67ee8bc94
gitE0Z9 <acrocanthosaurus627@gmail.com>
2023-12-09 23:53:11 +0800

    feat: extend recurrent classifier settings

--- torchlake/sequence_data/models/lstm/model.py
+++ torchlake/sequence_data/models/lstm/model.py
@@ -8,11 +8,14 @@ class LstmClassifier(nn.Module):
         vocab_size: int,
         embed_dim: int,
         hidden_dim: int,
+        output_size: int = 1,
         padding_idx: int | None = None,
         num_layers: int = 1,
         bidirectional: bool = False,
     ):
         super(LstmClassifier, self).__init__()
+        self.factor = 2 if bidirectional else 1
+
         self.embed = nn.Embedding(vocab_size, embed_dim, padding_idx=padding_idx)
         self.rnn = nn.LSTM(
             embed_dim,
@@ -21,15 +24,23 @@ class LstmClassifier(nn.Module):
             batch_first=True,
             bidirectional=bidirectional,
         )
-        self.fc = nn.Linear(hidden_dim, 1)
+        self.layer_norm = nn.LayerNorm(hidden_dim * self.factor)
+        self.fc = nn.Linear(hidden_dim * self.factor, output_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        output = self.embed(x)  # batch_size, seq_len, embed_dim
+        # batch_size, seq_len, embed_dim
+        y = self.embed(x)
+
+        # ot, (ht, ct)
         # ot: batch_size, seq_len, bidirectional*hidden_dim
         # ht: bidirectional * layer_size, batch_size, hidden_dim
-        _, (ht, _) = self.rnn(output)
+        ot, (ht, _) = self.rnn(y)
 
-        # the last layer's hidden state represents the paragraph
-        output = self.fc(ht[-1])
+        if self.fc.out_features <= 1:
+            # the last layer's hidden state represents the paragraph
+            y = self.fc(ht[-1])
+        else:
+            y = self.layer_norm(ot)
+            y = self.fc(y)
 
-        return output
+        return y
