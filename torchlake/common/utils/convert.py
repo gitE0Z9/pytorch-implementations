fac1b54ce64f7360baac7f1ec406a9428ee41df7
gitE0Z9 <acrocanthosaurus627@gmail.com>
2023-12-11 00:39:31 +0800

    feat: tensor2np for img

---
+++ torchlake/common/utils/convert.py
@@ -0,0 +1,17 @@
+import torch
+import numpy as np
+
+
+def tensor_to_np(tensor: torch.Tensor) -> np.ndarray:
+    if tensor.dim() == 3:
+        tensor: torch.Tensor = tensor.unsqueeze(0)
+
+    tensor = tensor.permute(0, 2, 3, 1)
+
+    if tensor.size(0) == 1:
+        tensor = tensor.squeeze(0)
+
+    if tensor.device.type != "cpu":
+        tensor = tensor.detach().cpu()
+
+    return tensor.numpy()
