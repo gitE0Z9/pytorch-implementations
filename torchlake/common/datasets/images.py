eb41d69ee3c91fa8c01af6c44fc3d6558191ec91
gitE0Z9 <acrocanthosaurus627@gmail.com>
2023-12-14 22:11:34 +0800

    fix: imagedataset glob pattern

--- torchlake/common/datasets/images.py
+++ torchlake/common/datasets/images.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+from glob import glob
 
 import torch
 from PIL import Image
@@ -6,8 +6,8 @@ from torch.utils.data import Dataset
 
 
 class ImageDataset(Dataset):
-    def __init__(self, root: str, transform=None):
-        self.path = list(Path(root).glob("*.jpg"))
+    def __init__(self, pattern: str, transform=None):
+        self.path = list(glob(pattern))
         self.transform = transform
 
     def __len__(self):
