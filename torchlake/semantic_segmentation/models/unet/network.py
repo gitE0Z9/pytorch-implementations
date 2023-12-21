b6a32984f271e31ab006b24c1a95493ed21e76d6
gitE0Z9 <47134502+gitE0Z9@users.noreply.github.com>
2023-12-21 18:10:17 +0800

    feat: mirror padding for unet

--- torchlake/semantic_segmentation/models/unet/network.py
+++ torchlake/semantic_segmentation/models/unet/network.py
@@ -34,13 +34,13 @@ class DownSampling(nn.Module):
 class UpSampling(nn.Module):
     def __init__(self, in_ch: int, out_ch: int):
         super(UpSampling, self).__init__()
-        self.up = nn.ConvTranspose2d(in_ch, int(in_ch / 2), 2, stride=2)
+        self.up = nn.ConvTranspose2d(in_ch, in_ch // 2, 2, stride=2)
         self.convs = DoubleConv(in_ch, out_ch)
 
     def forward(self, x: torch.Tensor, feature: torch.Tensor) -> torch.Tensor:
         y = self.up(x)
-        x_pad, y_pad = feature.size(3) - y.size(3), feature.size(2) - y.size(2)
-        y = nn.ZeroPad2d((0, 0, y_pad // 2, x_pad // 2))(y)
+        y_pad, x_pad = feature.size(3) - y.size(3), feature.size(2) - y.size(2)
+        y = nn.ReflectionPad2d((0, 0, y_pad // 2, x_pad // 2))(y)
         y = torch.cat([feature, y], dim=1)
         y = self.convs(y)
         return y
