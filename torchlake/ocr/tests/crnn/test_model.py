8273615b9c9c8cae09e0f89da473d3cdea086f38
gitE0Z9 <acrocanthosaurus627@gmail.com>
2023-12-24 22:49:06 +0800

    refactor: crnn test

--- torchlake/ocr/tests/crnn/test_model.py
+++
@@ -1,9 +0,0 @@
-import torch
-from ocr.models.crnn import Crnn
-
-
-def test_model_shape():
-    test_x = torch.randn(1, 3, 32, 1698)
-    output_shape = Crnn(4, 10)(test_x).shape
-
-    assert output_shape == torch.Size((1698 // 4 - 1, 1, 10))
