190bde627c05f23ac6673151c2228fcb86311cd3
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-03-03 00:59:07 +0800

    fix: add od data to pkg

--- setup.py
+++ setup.py
@@ -5,5 +5,14 @@ setup(
     name="torchlake",
     version="0.1.1",
     packages=find_packages(),
+    package_data={
+        "": [
+            "object_detection/configs/**/*.yml",
+            "object_detection/datasets/**/*.yml",
+            "object_detection/configs/**/*.txt",
+            "object_detection/datasets/**/*.txt",
+            "object_detection/datasets/**/*.csv",
+        ]
+    },
     install_requires=Path("requirements.txt").read_text().splitlines(),
 )
