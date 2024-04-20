cd77f49ccb3e2124cf4c9aee6989d6b380a152a9
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-04-20 23:43:32 +0800

    feat: common trainer

--- torchlake/common/controller/trainer.py
+++ torchlake/common/controller/trainer.py
@@ -1,4 +1,5 @@
-from typing import Iterator
+from abc import ABC
+from typing import Iterable, Iterator
 
 import torch
 from torch import nn
@@ -6,24 +7,28 @@ from torch.optim.optimizer import Optimizer
 from tqdm import tqdm
 
 
-class ClassificationTrainer:
+class TrainerBase(ABC):
     def __init__(self, epoches: int, device: torch.device):
         self.epoches = epoches
         self.device = device
 
-    @staticmethod
-    def get_criterion(label_size: int):
-        if label_size == 1:
-            return nn.BCEWithLogitsLoss()
-        elif label_size > 1:
-            return nn.CrossEntropyLoss()
-        else:
-            raise ValueError(f"label size {label_size} is not valid")
+    def get_criterion(self):
+        raise NotImplementedError
 
     @staticmethod
     def get_optimizer(name: str, *args, **kwargs) -> Optimizer:
         optim_class = getattr(torch.optim, name)
         return optim_class(*args, **kwargs)
+
+    def _predict(
+        self,
+        row: tuple[Iterable],
+        model: nn.Module,
+        criterion: nn.Module,
+        *args,
+        **kwargs,
+    ):
+        raise NotImplementedError
 
     def run(
         self,
@@ -31,6 +36,8 @@ class ClassificationTrainer:
         model: nn.Module,
         optimizer: Optimizer,
         criterion: nn.Module,
+        *args,
+        **kwargs,
     ):
         training_loss = []
 
@@ -39,7 +46,8 @@ class ClassificationTrainer:
             running_loss = 0.0
             data_size = 0
 
-            for x, y in tqdm(data):
+            for row in tqdm(data):
+                x = row[0]
                 if isinstance(x, torch.Tensor):
                     data_size += x.size(0)
                 elif isinstance(x, list):
@@ -47,12 +55,7 @@ class ClassificationTrainer:
 
                 optimizer.zero_grad()
 
-                x = x.to(self.device)
-                y = y.to(self.device)
-
-                output = model(x)
-
-                loss = criterion(output, y.long())
+                loss = self._predict(row, model, criterion, *args, **kwargs)
                 running_loss += loss.item()
 
                 loss.backward()
@@ -63,3 +66,32 @@ class ClassificationTrainer:
             print(f"epoch {e+1} : {running_loss / data_size}")
 
         return training_loss
+
+
+class ClassificationTrainer(TrainerBase):
+    @staticmethod
+    def get_criterion(label_size: int):
+        if label_size == 1:
+            return nn.BCEWithLogitsLoss()
+        elif label_size > 1:
+            return nn.CrossEntropyLoss()
+        else:
+            raise ValueError(f"label size {label_size} is not valid")
+
+    def _predict(
+        self,
+        row: tuple[Iterable],
+        model: nn.Module,
+        criterion: nn.Module,
+        *args,
+        **kwargs,
+    ):
+        x, y = row
+        x = x.to(self.device)
+        y = y.to(self.device)
+
+        output = model(x)
+
+        loss = criterion(output, y.long())
+
+        return loss
