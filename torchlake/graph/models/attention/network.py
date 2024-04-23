6e81946c9ea7caf16c35b0aa58e877e26516d0ed
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-04-24 00:41:20 +0800

    feat: gat v2

--- torchlake/graph/models/attention/network.py
+++ torchlake/graph/models/attention/network.py
@@ -1,58 +1,121 @@
 import torch
 from torch import nn
-from torch.nn.functional import leaky_relu_
+import torch.nn.functional as F
 
 
 class GatLayer(nn.Module):
     """ICLR 2018, GRAPH ATTENTION NETWORKS, Cambridge"""
 
-    def __init__(self, in_dim: int, out_dim: int, num_heads: int = 3):
+    def __init__(self, in_dim: int, latent_dim: int, num_heads: int = 3):
         super(GatLayer, self).__init__()
-        self.linear = nn.Linear(in_dim, out_dim, bias=False)
-        self.attention_layers = [
-            nn.Linear(out_dim * 2, out_dim * 2, bias=False) for _ in range(num_heads)
-        ]
+        self.input_layer = nn.Linear(in_dim, latent_dim, bias=False)
+        self.attention_vectors = nn.Parameter(torch.rand((num_heads, 2 * latent_dim)))
 
     def get_attention_weight(
         self,
-        attention_layer: nn.Module,
         h: torch.Tensor,
-        adj: torch.Tensor,
+        mask: torch.Tensor,
     ) -> torch.Tensor:
-        attention_weight = attention_layer(h) * adj
-        leaky_relu_(attention_weight)
+        """attention weight to neighbors
 
-        return attention_weight.softmax(dim=0)
+        Args:
+            h (torch.Tensor): node latent tensor, shape is (#edge, 2 * latent_dim)
+            mask (torch.Tensor): size of each node's neighborhood(include self), shape is (#node,)
 
-    def get_multihead_output(
+        Returns:
+            torch.Tensor: attention weight, shape is (#head, #edge)
+        """
+        # #head, #edge
+        attention_weight = torch.einsum("hf, ef -> he", self.attention_vectors, h)
+        attention_weight = F.leaky_relu(attention_weight, 0.2)
+
+        # TODO: masked
+        # #head, #edge
+        return attention_weight.softmax(dim=-1)
+
+    def get_output(
         self,
-        h_primes: list[torch.Tensor],
+        h: torch.Tensor,
         predict: bool = False,
     ) -> torch.Tensor:
+        """output as a prediction layer or a hidden layer
+
+        Args:
+            h (torch.Tensor): node latent tensor, shape is (#head, #node, #laten_dim)
+            predict (bool, optional): predict layer or not. Defaults to False.
+
+        Returns:
+            torch.Tensor: _description_
+        """
         if predict:
-            y = torch.stack(h_primes, dim=1)
-            y = y.mean(dim=1).sigmoid()
+            # #node, #latent_dim
+            y = h.mean(dim=0).sigmoid()
         else:
-            y = h_primes.sigmoid()
-            y = torch.stack(y, dim=1)
+            # #head, #node, #latent_dim
+            y = h.sigmoid()
 
         return y
 
     def forward(
         self,
         x: torch.Tensor,
-        adj: torch.Tensor,
+        edges: torch.Tensor,
+        a: torch.Tensor,
         predict: bool = False,
     ) -> torch.Tensor:
-        h = self.linear(x)
-        concated_h = torch.cat([h, h], -1)
+        """forward
 
-        h_primes = []
-        for attention_layer in self.attention_layers:
-            attention_weight = self.get_attention_weight(
-                attention_layer, concated_h, adj
-            )
-            h_prime = torch.mm(attention_weight, h)
-            h_primes.append(h_prime)
+        Args:
+            x (torch.Tensor): node features, shape is (#node, #feature)
+            edges (torch.Tensor): edges, shape is (#edge, 2)
+            a (torch.Tensor): adjacency matrix, shape is (#node, #node)
+            predict (bool, optional): predict layer or not. Defaults to False.
 
-        return self.get_multihead_output(h_primes, predict)
+        Returns:
+            torch.Tensor: output tensor, shape is (#node, #feature)
+        """
+        # #node, latent_dim
+        h = self.input_layer(x)
+        # #edge, 2 * latent_dim
+        h = h[edges, :].view(-1, 2 * h.size(-1))
+
+        # degree
+        degree = a.sum(0)
+        mask = (
+            torch.Tensor([d * [i] for i, d in enumerate(degree)]).view(-1).to(a.device)
+        )
+
+        # #head, #edge
+        attention_weight = self.get_attention_weight(h, mask)
+        # #edge, latent_dim
+        _, h_j = torch.split(h, 2, -1)
+
+        # TODO: masked
+        # #head, latent_dim
+        h = torch.einsum("he, ef -> hf", attention_weight, h_j)
+
+        return self.get_output(h, predict)
+
+
+class GatLayerV2(GatLayer):
+    """ICLR 2022, HOW ATTENTIVE ARE GRAPH ATTENTION NETWORKS, Technion, Cambridge"""
+
+    def __init__(self, in_dim: int, latent_dim: int, num_heads: int = 3):
+        super(GatLayerV2, self).__init__(in_dim, latent_dim, num_heads)
+
+    def get_attention_weight(self, h: torch.Tensor) -> torch.Tensor:
+        """attention weight to neighbors
+
+        Args:
+            h (torch.Tensor): node latent tensor, shape is (#edge, 2 * latent_dim)
+
+        Returns:
+            torch.Tensor: attention weight, shape is (#head, #edge)
+        """
+        # #head, #edge
+        attention_weight = F.leaky_relu(h, 0.2)
+        attention_weight = torch.einsum("hf, ef -> he", self.attention_vectors, h)
+
+        # TODO: masked
+        # #head, #edge
+        return attention_weight.softmax(dim=-1)
