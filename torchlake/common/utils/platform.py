e4c41f1a5e2ccc3584c94a223c64ff66ec6514fc
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-05-02 01:34:56 +0800

    feat: get file size

--- torchlake/common/utils/platform.py
+++ torchlake/common/utils/platform.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+from typing import Literal
 import platform
 
 
@@ -7,3 +9,20 @@ def get_num_workers() -> int:
         return 0
     else:
         return os.cpu_count()
+
+
+def get_file_size(
+    path: str | Path,
+    unit: Literal["K"] | Literal["M"] | Literal["G"] = "M",
+) -> float:
+    if isinstance(path, str):
+        path = Path(path)
+
+    mapping = {
+        "K": 1024,
+        "M": 1024**2,
+        "G": 1024**3,
+    }
+    divider = mapping[unit]
+
+    return round(path.stat().st_size / divider, 2) + f"{unit}B"
