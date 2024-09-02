fa2d4b37c1a332485f3467c57c1507142ecdfaf9
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-09-02 21:51:45 +0800

    fix: cnn base init

--- torchlake/common/models/cnn_base.py
+++ torchlake/common/models/cnn_base.py
@@ -6,13 +6,14 @@ from torch import nn
 from torchlake.common.models.flatten import FlattenFeature
 
 
-class ModelBase(ABC):
+class ModelBase(nn.Module, ABC):
 
     def __init__(
         self,
         input_channel: int = 3,
         output_size: int = 1,
     ):
+        super(ModelBase, self).__init__()
         self.build_foot(input_channel)
         self.build_blocks()
         self.build_head(output_size)
@@ -34,12 +35,9 @@ class ModelBase(ABC):
         self.blocks = ...
 
     def build_head(self, output_size: int):
-        self.pool = FlattenFeature()
-        self.fc = nn.Linear(self.feature_dim, output_size)
-
         self.head = nn.Sequential(
-            self.pool,
-            self.fc,
+            FlattenFeature(),
+            nn.Linear(self.feature_dim, output_size),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
