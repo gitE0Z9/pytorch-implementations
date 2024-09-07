3bc61062e600e2fb0a72ac5901a47585a01bb574
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-09-08 00:28:05 +0800

    feat: csv dataset

--- torchlake/common/datasets/csv.py
+++ torchlake/common/datasets/csv.py
@@ -1,16 +1,34 @@
 import pandas as pd
 from torch.utils.data import Dataset
+from pathlib import Path
 
 
 class CsvDataset(Dataset):
-    def __init__(self, path: str | list[str]):
-        self.path = path
+    def __init__(
+        self,
+        paths: list[str | Path],
+        feature_columns: list[str],
+        target_column: str,
+    ):
+        self.paths = paths
+        self.feature_columns = feature_columns
+        self.target_column = target_column
+        self.data: list[pd.DataFrame] = []
+        self.count = 0
 
-        for path in self.path:
-            print("reading from", path, "...")
+        columns = [*self.feature_columns, *self.target_column]
+        self.data = []
+        for file_path in self.paths:
+            print(f"Reading from {file_path}...")
+            df = pd.read_csv(file_path, usecols=columns)
+            self.data.append(df)
+            self.count += len(df)
+        self.data = pd.concat(self.data, ignore_index=True)
 
     def __len__(self) -> int:
-        return len(self.labels)
+        return self.count
 
-    def __getitem__(self, idx: int):
-        ...
+    def __getitem__(self, idx: int) -> tuple:
+        features = self.data.loc[idx, self.feature_columns].values
+        target = self.data.loc[idx, self.target_column].values
+        return features, target
