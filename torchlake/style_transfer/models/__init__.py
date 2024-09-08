a551742769513b82ca75b856d5442e56bd5b45ec
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-09-08 21:18:38 +0800

    fix: style transfer export

--- torchlake/style_transfer/models/__init__.py
+++ torchlake/style_transfer/models/__init__.py
@@ -1,23 +1,14 @@
 from .neural_style_transfer.model import NeuralStyleTransfer
-from .neural_style_transfer.loss import NeuralStyleTransferLoss
-from .neural_doodle.loss import MrfLoss
-from .neural_doodle.model import AuxiliaryNetwork
+from .neural_doodle.model import AuxiliaryNetwork as DoodleAuxiliaryNetwork
 from .fast_style_transfer.model import FastStyleTransfer
 from .pix2pix.model import Pix2PixDiscriminator, Pix2PixGenerator
-from .pix2pix.loss import Pix2PixDiscriminatorLoss, Pix2PixGeneratorLoss
 from .adain.model import AdaInTrainer
-from .adain.loss import AdaInLoss
 
 __all__ = [
     "NeuralStyleTransfer",
-    "NeuralStyleTransferLoss",
-    "AuxiliaryNetwork",
-    "MrfLoss",
+    "DoodleAuxiliaryNetwork",
     "FastStyleTransfer",
     "Pix2PixDiscriminator",
     "Pix2PixGenerator",
-    "Pix2PixDiscriminatorLoss",
-    "Pix2PixGeneratorLoss",
     "AdaInTrainer",
-    "AdaInLoss",
 ]
