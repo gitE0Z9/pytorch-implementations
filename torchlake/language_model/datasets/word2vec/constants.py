a8107c4a6e5968ad8696dc5119582386ed00b86a
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-09-14 23:22:42 +0800

    chore: word2vec analogy sample constants

---
+++ torchlake/language_model/datasets/word2vec/constants.py
@@ -0,0 +1,11 @@
+WORD_ANALOGY_SAMPLE = [
+    ["paris", "france"],
+    ["beijing", "china"],
+    ["moscow", "russia"],
+    ["tokyo", "japan"],
+    ["athens", "greece"],
+    ["rome", "italy"],
+    ["berlin", "germany"],
+    ["madrid", "spain"],
+    ["lisbon", "portugal"],
+]
