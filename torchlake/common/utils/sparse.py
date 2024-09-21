56599ab3d8e63c20b7b1bfa7faf089a15f2af333
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-09-21 16:23:56 +0800

    fix/sparse: ones_tensor got size argument to ensure output shape

--- torchlake/common/utils/sparse.py
+++ torchlake/common/utils/sparse.py
@@ -7,13 +7,14 @@ def eye_matrix(rank: int) -> torch.Tensor:
     return torch.sparse_coo_tensor(indices.repeat(2, 1), torch.ones_like(indices))
 
 
-def ones_tensor(indices: torch.Tensor) -> torch.Tensor:
+def ones_tensor(indices: torch.Tensor, size: torch.Size | None = None) -> torch.Tensor:
     assert indices.size(0) == 2, "indices shape is (2, edge)"
     assert indices.size(1) > 0, "indices shape is (2, edge)"
 
     return torch.sparse_coo_tensor(
         indices,
         torch.ones(indices.size(1)).to(indices.device),
+        size=size,
     )
 
 
