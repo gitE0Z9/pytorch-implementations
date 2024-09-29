387bbb7b38d882eaa0434152e3e16b0be01f9c6e
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-09-29 22:58:11 +0800

    feat/common: extend predict mixin with predict_nothing mode

--- torchlake/common/mixins/controller.py
+++ torchlake/common/mixins/controller.py
@@ -5,12 +5,42 @@ from torch import nn
 
 
 class PredictFunctionMixin:
+    def set_predict_strategy(
+        self,
+        is_x_collection: bool = False,
+        nested_level: int = 0,
+    ):
+        if not is_x_collection:
+            self._predict = self._predict_with_tensor
+            return
+        else:
+            assert (
+                nested_level > 0
+            ), "if x is a collection, please tell us nested level of tensors"
+
+            if nested_level == 1:
+                self._predict = self._predict_with_tensors
+                return
+
+            if nested_level > 1:
+                print(
+                    "for deeply nested tensors, please send to device in collate_fn or earlier stage, we will pass x as it is"
+                )
+                self._predict = self._predict_do_nothing
+
     def build_predict_function_by_data_type(self, data: Iterable):
         x, _ = next(data)
-        if isinstance(x, list):
-            self._predict = self._predict_with_tensors
-        else:
+        is_x_collection = isinstance(x, list | tuple | set)
+
+        if not is_x_collection:
             self._predict = self._predict_with_tensor
+            return
+        else:
+            is_x_tensor = isinstance(x[0], torch.Tensor)
+            if is_x_tensor:
+                self._predict = self._predict_with_tensors
+            else:
+                self._predict = self._predict_do_nothing
 
     def _predict_with_tensor(
         self,
@@ -34,4 +64,14 @@ class PredictFunctionMixin:
         for i in range(len(x)):
             x[i] = x[i].to(self.device)
 
+        return model(*x, *args, **kwargs)
+
+    def _predict_do_nothing(
+        self,
+        row: tuple[Iterable],
+        model: nn.Module,
+        *args,
+        **kwargs,
+    ) -> torch.Tensor | Any:
+        x, _ = row
         return model(*x, *args, **kwargs)
