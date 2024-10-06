51f4c2dc9d35fe0ea93db4fdbc1ea54f95694d9b
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-10-06 23:11:10 +0800

    refactor: readable num skip connection in fcn

--- torchlake/semantic_segmentation/models/fcn/model.py
+++ torchlake/semantic_segmentation/models/fcn/model.py
@@ -11,14 +11,14 @@ class FCNLegacy(nn.Module):
     def __init__(
         self,
         output_size: int = 1,
-        num_skip_connection: int = 3,
+        num_skip_connection: int = 2,
         frozen_backbone: bool = False,
     ):
         """Fully Convolutional Networks for Semantic Segmentation in paper [1411.4038v2]
 
         Args:
             output_size (int, optional): output size. Defaults to 1.
-            num_skip_connection (int, optional): number of skip connection, e.g. 3 means FCN-8s, 2 means FCN-16s, 1 means FCN-32s. Defaults to 3.
+            num_skip_connection (int, optional): number of skip connection, e.g. 2 means FCN-8s, 1 means FCN-16s, 0 means FCN-32s. Defaults to 2.
             fronzen_backbone (bool, optional): froze the vgg backbone or not. Defaults to False.
         """
         super(FCNLegacy, self).__init__()
@@ -39,23 +39,23 @@ class FCNLegacy(nn.Module):
                         (512, 256),  # pool4 -> pool3
                         (256, 128),  # pool3 -> pool2
                         (128, 64),  # pool2 -> pool1
-                    )[: num_skip_connection - 1]
+                    )[:num_skip_connection]
                 ],
                 # final upsample
                 UpSamplingLegacy(
-                    [512, 512, 256][num_skip_connection - 1],  # 32s, 16s, 8s
+                    [512, 512, 256][num_skip_connection],  # 32s, 16s, 8s
                     output_size,
                     # e.g. 3 => 8s => 8, 2 => 16s => 16
-                    kernel=2 ** (6 - num_skip_connection),
+                    kernel=2 ** (5 - num_skip_connection),
                     # e.g. 3 => 8s => 8, 2 => 16s => 16
-                    stride=2 ** (6 - num_skip_connection),
+                    stride=2 ** (5 - num_skip_connection),
                 ),
             ]
         )
         self.fc = nn.Conv2d(output_size, output_size, 1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        layers_no = [f"{i}_1" for i in range(5, 5 - self.num_skip_connection, -1)]
+        layers_no = [f"{i}_1" for i in range(5, 4 - self.num_skip_connection, -1)]
         features = self.backbone(x, layers_no)
 
         y = features.pop()  # 32x, 512
@@ -70,14 +70,14 @@ class FCN(nn.Module):
     def __init__(
         self,
         output_size: int = 1,
-        num_skip_connection: int = 3,
+        num_skip_connection: int = 2,
         frozen_backbone: bool = False,
     ):
         """Fully Convolutional Networks for Semantic Segmentation in paper [1605.06211v1]
 
         Args:
             output_size (int, optional): output size. Defaults to 1.
-            num_skip_connection (int, optional): number of skip connection, e.g. 3 means FCN-8s, 2 means FCN-16s, 1 means FCN-32s. Defaults to 3.
+            num_skip_connection (int, optional): number of skip connection, e.g. 2 means FCN-8s, 1 means FCN-16s, 0 means FCN-32s. Defaults to 2.
             fronzen_backbone (bool, optional): froze the vgg backbone or not. Defaults to False.
         """
         super(FCN, self).__init__()
@@ -88,32 +88,26 @@ class FCN(nn.Module):
             nn.Dropout(p=0.5),
             Conv2dNormActivation(4096, 4096, 1, padding=0, norm_layer=None),
             nn.Dropout(p=0.5),
+            nn.Conv2d(4096, output_size, 1),
         )
         self.pool_convs = nn.ModuleList(
             [
-                # nn.Conv2d(512, output_size, 1),
-                nn.Conv2d(4096, output_size, 1),
-                *[
-                    nn.Conv2d(dim, output_size, 1)
-                    for dim in [512, 512, 256, 128, 64][1:num_skip_connection]
-                ],
+                nn.Conv2d(dim, output_size, 1)
+                for dim in [512, 256, 128, 64][:num_skip_connection]
             ]
         )
+        # intermediate upsample
         self.upsamples = nn.ModuleList(
-            [
-                # intermediate upsample
-                *[UpSampling(output_size, output_size, 4, stride=2)]
-                * (num_skip_connection - 1),
-                # final upsample
-                UpSampling(
-                    output_size,
-                    output_size,
-                    # e.g. 3 => 8s => 16, 2 => 16s => 32
-                    kernel=2 ** (7 - num_skip_connection),
-                    # e.g. 3 => 8s => 8, 2 => 16s => 16
-                    stride=2 ** (6 - num_skip_connection),
-                ),
-            ]
+            [UpSampling(output_size, output_size, 4, stride=2)] * num_skip_connection
+        )
+        # final upsample
+        self.upsample_head = UpSampling(
+            output_size,
+            output_size,
+            # e.g. 2 => 8s => 16, 1 => 16s => 32
+            kernel=2 ** (6 - num_skip_connection),
+            # e.g. 2 => 8s => 8, 1 => 16s => 16
+            stride=2 ** (5 - num_skip_connection),
         )
 
     def build_backbone(self, frozen_backbone: bool) -> VGGFeatureExtractor:
@@ -131,15 +125,14 @@ class FCN(nn.Module):
         # 8s => 5, 4, 3
         # 16s => 5, 4
         # 32s => 5
-        layers_no = [f"{i}_1" for i in range(5, 5 - self.num_skip_connection, -1)]
+        layers_no = [f"{i}_1" for i in range(5, 4 - self.num_skip_connection, -1)]
         features = self.backbone(x, layers_no)
 
         # feature fusion
-        y = self.pool_convs[0](self.conv(features.pop()))  # 32x
-        # y = self.pool_convs[0](features.pop())  # 32x
-        for upsample, pool_conv in zip(self.upsamples[:-1], self.pool_convs[1:]):
+        y = self.conv(features.pop())  # 32x
+        for upsample, pool_conv in zip(self.upsamples, self.pool_convs):
             y = upsample(y)
             y += CenterCrop(y.shape[-2:])(pool_conv(features.pop()))
 
-        y = self.upsamples[-1](y)
+        y = self.upsample_head(y)
         return CenterCrop(x.shape[-2:])(y)
--- torchlake/semantic_segmentation/tests/test_fcn.py
+++ torchlake/semantic_segmentation/tests/test_fcn.py
@@ -4,7 +4,7 @@ import torch
 from ..models.fcn.model import FCN, FCNLegacy
 
 
-@pytest.mark.parametrize("num_skip_connection", [1, 2, 3])
+@pytest.mark.parametrize("num_skip_connection", [0, 1, 2])
 def test_legacy_forward_shape(num_skip_connection: int):
     x = torch.rand((1, 3, 32, 32))
 
@@ -18,7 +18,7 @@ def test_legacy_forward_shape(num_skip_connection: int):
     assert y.shape == torch.Size((1, 21, 32, 32))
 
 
-@pytest.mark.parametrize("num_skip_connection", [1, 2, 3])
+@pytest.mark.parametrize("num_skip_connection", [0, 1, 2])
 def test_forward_shape(num_skip_connection: int):
     x = torch.rand((1, 3, 32, 32))
 
