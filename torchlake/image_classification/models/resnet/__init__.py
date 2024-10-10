00229cc258af425a39da66367eb1ddcb8b08e6bc
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-10-10 22:02:50 +0800

    refactor: resnet  submodule init

---
+++ torchlake/image_classification/models/resnet/__init__.py
@@ -0,0 +1,22 @@
+from .model import ResNet, ResNetB, ResNetC, ResNetD
+from .network import (
+    ConvBlock,
+    BottleNeck,
+    BottleNeckB,
+    BottleNeckD,
+    ResBlock,
+    ResBlockD,
+)
+
+__all__ = [
+    "ResNet",
+    "ResNetB",
+    "ResNetC",
+    "ResNetD",
+    "ConvBlock",
+    "BottleNeck",
+    "BottleNeckB",
+    "BottleNeckD",
+    "ResBlock",
+    "ResBlockD",
+]
