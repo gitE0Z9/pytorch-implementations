9efc5e9cd295e3d3b1c9210c2d63fc6bf9b6d4a4
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-10-10 15:11:13 +0800

    feat (mobilenet fe): hardcoded dim and stage index

--- torchlake/common/models/mobilenet_feature_extractor.py
+++ torchlake/common/models/mobilenet_feature_extractor.py
@@ -35,6 +35,56 @@ class MobileNetFeatureExtractor(ExtractorBase):
         super().__init__(network_name, layer_type, trainable)
         self.normalization = ImageNetNormalization()
 
+    def get_feature_dim(self) -> int:
+        return {
+            "mobilenet_v2": [
+                [32, 16],
+                [24] * 2,
+                [32] * 3,
+                [64] * 4 + [96] * 3,
+                [160] * 3 + [320] + [1280],
+            ],
+            "mobilenet_v3_small": [
+                [16],
+                [16],
+                [24],
+                [40] * 3 + [48] * 2,
+                [96] * 3 + [576],
+            ],
+            "mobilenet_v3_large": [
+                [16, 16],
+                [24] * 2,
+                [40] * 3,
+                [80] * 4 + [112] * 2,
+                [160] * 3 + [960],
+            ],
+        }[self.network_name]
+
+    def get_stage(self) -> list[list[int]]:
+        return {
+            "mobilenet_v2": [
+                [0, 1],
+                [2, 3],
+                [4, 5, 6],
+                [7, 8, 9, 10, 11, 12, 13],
+                [14, 15, 16, 17, 18],
+            ],
+            "mobilenet_v3_small": [
+                [0],
+                [1],
+                [2, 3],
+                [4, 5, 6, 7, 8],
+                [9, 10, 11, 12],
+            ],
+            "mobilenet_v3_large": [
+                [0, 1],
+                [2, 3],
+                [4, 5, 6],
+                [7, 8, 9, 10, 11, 12],
+                [13, 14, 15, 16],
+            ],
+        }[self.network_name]
+
     def get_weight(self, network_name: str) -> Weights:
         return {
             "mobilenet_v2": MobileNet_V2_Weights.DEFAULT,
@@ -65,22 +115,14 @@ class MobileNetFeatureExtractor(ExtractorBase):
         img = self.normalization(img)
 
         y = img
-        block_count = -1
-        layer_count = 0
-        for layer in self.feature_extractor:
-            layer_count += 1
+        for stage_idx, stage in enumerate(self.get_stage()):
+            for layer_count, layer_idx in enumerate(stage):
+                layer = self.feature_extractor[layer_idx]
+                y: torch.Tensor = layer(y)
 
-            input_shape = y.shape
-            y: torch.Tensor = layer(y)
-            output_shape = y.shape
-
-            if input_shape[2:] != output_shape[2:]:
-                block_count += 1
-                layer_count = 1
-
-            layer_name = f"{block_count}_{layer_count}"
-            if layer_name in target_layer_names:
-                features.append(y)
+                layer_name = f"{stage_idx}_{layer_count+1}"
+                if layer_name in target_layer_names:
+                    features.append(y)
 
         if "output" in target_layer_names:
             features.append(y.mean((2, 3)))
