074f6fd2168d86c6ee44b13b1905992a856ced45
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-10-12 12:09:28 +0800

    feat: mobilenet dilated backbone mixin

--- torchlake/semantic_segmentation/models/lr_aspp/model.py
+++ torchlake/semantic_segmentation/models/lr_aspp/model.py
@@ -3,13 +3,14 @@ from typing import Literal
 
 import torch
 from torch import nn
+from torchlake.common.models import MobileNetFeatureExtractor
 from torchvision.transforms import CenterCrop
 
-from torchlake.common.models import MobileNetFeatureExtractor
+from ...mixins.mobilenet_backbone import MobileNetBackboneMixin
 from .network import LRASPP
 
 
-class MobileNetV3Seg(nn.Module):
+class MobileNetV3Seg(MobileNetBackboneMixin, nn.Module):
 
     def __init__(
         self,
@@ -38,6 +39,13 @@ class MobileNetV3Seg(nn.Module):
         self.backbone: MobileNetFeatureExtractor = self.build_backbone(
             backbone_name, frozen_backbone
         )
+        self.backbone.forward = partial(
+            self.backbone.forward,
+            target_layer_names=[
+                f"2_{len(self.backbone.get_stage()[-3])}",
+                f"4_{len(self.backbone.get_stage()[-1])-1}",
+            ],
+        )
         self.feature_dims = [
             self.backbone.get_feature_dim()[-3][-1],
             self.backbone.get_feature_dim()[-1][-2],
@@ -49,69 +57,6 @@ class MobileNetV3Seg(nn.Module):
             pool_stride,
         )
         self.upsample = nn.Upsample(scale_factor=8, mode="bilinear", align_corners=True)
-
-    def build_backbone(
-        self,
-        backbone_name: Literal[
-            "mobilenet_v2",
-            "mobilenet_v3_small",
-            "mobilenet_v3_large",
-        ],
-        frozen_backbone: bool,
-    ) -> MobileNetFeatureExtractor:
-        """build backbone, we use mobilenet
-
-        Args:
-            backbone_name (Literal[ "mobilenet_v2", "mobilenet_v3_small", "mobilenet_v3_large"], optional): mobilenet network name.
-            fronzen_backbone (bool, optional): froze the backbone or not.
-
-        Returns:
-            MobileNetFeatureExtractor: feature extractor
-        """
-        backbone = MobileNetFeatureExtractor(
-            backbone_name,
-            "block",
-            trainable=not frozen_backbone,
-        )
-        feature_extractor = backbone.feature_extractor
-        block_indices = backbone.get_stage()
-
-        # please ignore coding redundancy
-        for index in block_indices[-2]:
-            for key, layer in feature_extractor[index].named_modules():
-                layer: nn.Conv2d
-                if "block.1.0" in key or "conv.1.0" in key:
-                    kernel = layer.kernel_size[0]
-                    dilation = 2
-                    pad = (dilation * (kernel - 1)) // 2
-
-                    layer.dilation, layer.padding = (2, 2), (pad, pad)
-
-        penultimate_index = block_indices[-1]
-        for index in penultimate_index:
-            for key, layer in feature_extractor[index].named_modules():
-                layer: nn.Conv2d
-                if "block.1.0" in key or "conv.1.0" in key:
-                    # o = i - d(k-1) + 2p
-                    kernel = layer.kernel_size[0]
-                    dilation = 4
-                    pad = (dilation * (kernel - 1)) // 2
-
-                    layer.dilation, layer.padding, layer.stride = (
-                        (4, 4),
-                        (pad, pad),  # +0 for v2, +2 for v3-S, +4 for v3-L
-                        (1, 1),
-                    )
-
-        backbone.forward = partial(
-            backbone.forward,
-            target_layer_names=[
-                f"2_{len(block_indices[-3])}",
-                f"4_{len(penultimate_index)-1}",
-            ],
-        )
-
-        return backbone
 
     def build_head(
         self,
--- torchlake/semantic_segmentation/models/r_aspp/model.py
+++ torchlake/semantic_segmentation/models/r_aspp/model.py
@@ -3,13 +3,14 @@ from typing import Literal
 
 import torch
 from torch import nn
+from torchlake.common.models import MobileNetFeatureExtractor
 from torchvision.transforms import CenterCrop
 
-from torchlake.common.models import MobileNetFeatureExtractor
+from ...mixins.mobilenet_backbone import MobileNetBackboneMixin
 from ..deeplabv3.network import ASPP
 
 
-class MobileNetV2Seg(nn.Module):
+class MobileNetV2Seg(MobileNetBackboneMixin, nn.Module):
 
     def __init__(
         self,
@@ -35,56 +36,14 @@ class MobileNetV2Seg(nn.Module):
         self.backbone: MobileNetFeatureExtractor = self.build_backbone(
             backbone_name, frozen_backbone
         )
+        self.backbone.forward = partial(
+            self.backbone.forward,
+            target_layer_names=[f"4_{len(self.backbone.get_stage()[-1])-1}"],
+        )
+        self.feature_dim = self.backbone.get_feature_dim()[-1][-2]
         self.neck = self.build_neck()
         self.head = nn.Conv2d(self.feature_dim // self.reduction_ratio, output_size, 1)
         self.upsample = nn.Upsample(scale_factor=8, mode="bilinear", align_corners=True)
-
-    def build_backbone(
-        self,
-        backbone_name: Literal[
-            "mobilenet_v2",
-            "mobilenet_v3_small",
-            "mobilenet_v3_large",
-        ],
-        frozen_backbone: bool,
-    ) -> MobileNetFeatureExtractor:
-        """build backbone, we use mobilenet
-
-        Args:
-            backbone_name (Literal[ "mobilenet_v2", "mobilenet_v3_small", "mobilenet_v3_large"], optional): mobilenet network name.
-            fronzen_backbone (bool, optional): froze the backbone or not.
-
-        Returns:
-            MobileNetFeatureExtractor: feature extractor
-        """
-        backbone = MobileNetFeatureExtractor(
-            backbone_name,
-            "block",
-            trainable=not frozen_backbone,
-        )
-        feature_extractor = backbone.feature_extractor
-        block_indices = backbone.get_stage()
-
-        # please ignore coding redundancy
-        for index in block_indices[-2]:
-            for key, layer in feature_extractor[index].named_modules():
-                layer: nn.Conv2d
-                if "conv.1.0" in key:
-                    layer.dilation, layer.padding, layer.stride = (2, 2), (2, 2), (1, 1)
-
-        penultimate_index = block_indices[-1]
-        for index in penultimate_index:
-            for key, layer in feature_extractor[index].named_modules():
-                layer: nn.Conv2d
-                if "conv.1.0" in key:
-                    layer.dilation, layer.padding, layer.stride = (4, 4), (4, 4), (1, 1)
-
-        backbone.forward = partial(
-            backbone.forward, target_layer_names=[f"4_{len(penultimate_index)-1}"]
-        )
-        self.feature_dim = backbone.get_feature_dim()[-1][-2]
-
-        return backbone
 
     def build_neck(self) -> nn.Module:
         """ASPP withotut 3x3
