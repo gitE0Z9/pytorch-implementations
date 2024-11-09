3c70066fa56fd7e52c5ee4ae98e3fe3633152cdc
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-11-09 23:11:28 +0800

    feat: img from tensor to numpy add scale, output type, and contiguous assurance

--- torchlake/common/utils/convert.py
+++ torchlake/common/utils/convert.py
@@ -2,7 +2,11 @@ import torch
 import numpy as np
 
 
-def img_tensor_to_np(tensor: torch.Tensor) -> np.ndarray:
+def img_tensor_to_np(
+    tensor: torch.Tensor,
+    scale: float = 1,
+    output_type=np.float32,
+) -> np.ndarray:
     if tensor.dim() == 3:
         tensor: torch.Tensor = tensor.unsqueeze(0)
 
@@ -14,4 +18,9 @@ def img_tensor_to_np(tensor: torch.Tensor) -> np.ndarray:
     if tensor.device.type != "cpu":
         tensor = tensor.detach().cpu()
 
-    return tensor.numpy()
+    if scale != 1:
+        tensor *= scale
+
+    arr = tensor.numpy().astype(output_type)
+
+    return np.ascontiguousarray(arr)
