56e1ee2d73301764e847ed16f15f4dabeba5565f
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-11-19 22:46:57 +0800

    test: reghead

--- torchlake/object_detection/tests/test_models_base_network.py
+++ torchlake/object_detection/tests/test_models_base_network.py
@@ -1,13 +1,14 @@
 import torch
 from torch.testing import assert_close
 
-from ..models.base.network import ConvBlock
+from ..models.base.network import RegHead
 
 
-class TestConvBlock:
+class TestRegHead:
     def test_output_shape(self):
-        model = ConvBlock(3, 64, 3)
-        test_x = torch.rand(2, 3, 224, 224)
+        model = RegHead(16, 20, 5, 4)
+        test_x = torch.rand(2, 16, 7, 7)
 
-        output: torch.Tensor = model(test_x)
-        assert_close(output.shape, torch.Size([2, 64, 224, 224]))
+        coord, conf = model(test_x)
+        assert_close(coord.shape, torch.Size([2, 4 * 5, 7, 7]))
+        assert_close(conf.shape, torch.Size([2, 20 * 5, 7, 7]))
