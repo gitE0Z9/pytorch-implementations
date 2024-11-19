177a18d8a86ceb91b5094f90dd51f1ad7b7d82d7
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-11-19 21:34:17 +0800

    fix

--- torchlake/common/models/__init__.py
+++ torchlake/common/models/__init__.py
@@ -6,14 +6,15 @@ from .highway import HighwayBlock
 from .imagenet_normalization import ImageNetNormalization
 from .kernel_pca import KernelPCA
 from .kmeans import KMeans
+from .l2_norm import L2Norm
+from .mobilenet_feature_extractor import MobileNetFeatureExtractor
+from .multikernel_conv import MultiKernelConvModule
+from .position_encoding import PositionEncoding
 from .residual import ResBlock
+from .resnet_feature_extractor import ResNetFeatureExtractor
 from .se import SqueezeExcitation2d
 from .topk_pool import KmaxPool1d
-from .multikernel_conv import MultiKernelConvModule
 from .vgg_feature_extractor import VGGFeatureExtractor
-from .resnet_feature_extractor import ResNetFeatureExtractor
-from .mobilenet_feature_extractor import MobileNetFeatureExtractor
-from .l2_norm import L2Norm
 
 __all__ = [
     "SqueezeExcitation2d",
@@ -32,4 +33,5 @@ __all__ = [
     "KMeans",
     "MultiKernelConvModule",
     "L2Norm",
+    "PositionEncoding",
 ]
