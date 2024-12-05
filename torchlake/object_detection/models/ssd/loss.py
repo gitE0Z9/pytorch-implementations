f453edfa70683ad0fe6f63f4637c40daff3f3db2
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-12-05 21:13:53 +0800

    refactor (multibox loss): correct ranking & per image number of positive, but performance drop :(

--- torchlake/object_detection/models/ssd/loss.py
+++ torchlake/object_detection/models/ssd/loss.py
@@ -36,6 +36,9 @@ def hard_negative_mining(
         max=num_boxes - 1,
     )
 
+    num_positive.squeeze_((-2, -1))
+    remained_batch_indices = num_positive > 0
+
     # B, A, 1
     cls_loss = F.cross_entropy(
         conf_pred.permute(0, 2, 1),
@@ -43,10 +46,10 @@ def hard_negative_mining(
         reduction="none",
     ).unsqueeze(-1)
 
-    # B*A [B*A] => 1
-    positive_loss = cls_loss.view(-1)[
-        positive_mask.view(-1).nonzero().squeeze_(-1)
-    ].sum()
+    # B*A [B*A] => ?
+    positive_loss = cls_loss.view(-1)[positive_mask.view(-1).nonzero().squeeze_(-1)]
+
+    positive_loss /= num_positive.repeat_interleave(num_positive)
 
     # Hard Negative Mining
 
@@ -58,7 +61,7 @@ def hard_negative_mining(
         # B, A, 1
         _, loss_idx = all_negative_loss.sort(1, descending=True)
         # B, A, 1
-        _, loss_rank = loss_idx.sort()
+        _, loss_rank = loss_idx.sort(1)
         # B*A => ?
         negative_indices = (loss_rank < num_negative).view(-1).nonzero().squeeze_(-1)
 
@@ -68,10 +71,12 @@ def hard_negative_mining(
         # ?
         # 0 is background class
         torch.zeros_like(negative_indices),
-        reduction="sum",
+        reduction="none",
     )
 
-    return positive_loss + negative_loss
+    negative_loss /= num_positive.repeat_interleave(num_negative.view(-1))
+
+    return positive_loss.sum() + negative_loss.sum()
 
 
 class MultiBoxLoss(nn.Module):
@@ -224,26 +229,32 @@ class MultiBoxLoss(nn.Module):
         # unmatched will gain 0 as background
         target = self.match(gt, spans, self.anchors)
 
-        # batch size, 8732, 1
-        positive_mask = target[:, :, 4:5].gt(0).long()
+        # batch size, 8732
+        positive_mask = target[:, :, 4].gt(0).long()
 
-        N = positive_mask.sum()
-        if N <= 0:
-            return 0
+        # batch size
+        N = positive_mask.sum(1)
+        remained_batch_indices = N > 0
 
+        # ? (all positive indices)
         positive_indices = positive_mask.view(-1).nonzero().squeeze_(-1)
+        # ?, 4
         loc_loss = F.smooth_l1_loss(
-            loc_pred.view(-1, 4)[positive_indices],
-            target[:, :, :4].view(-1, 4)[positive_indices],
-            reduction="sum",
+            loc_pred[remained_batch_indices].view(-1, 4)[positive_indices],
+            target[remained_batch_indices, :, :4].view(-1, 4)[positive_indices],
+            reduction="none",
         )
 
+        # 1
+        loc_loss = torch.sum(loc_loss / N.repeat_interleave(N)[:, None])
+
+        # 1
         cls_loss = hard_negative_mining(
             conf_pred,
             target[:, :, 4],
-            positive_mask,
+            positive_mask.unsqueeze(-1),
             self.negative_ratio,
         )
 
         # Sum of losses: L = (L_conf + α L_loc) / N
-        return (loc_loss + self.alpha * cls_loss) / N
+        return self.alpha * loc_loss + cls_loss
