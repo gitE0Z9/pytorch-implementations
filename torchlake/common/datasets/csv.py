c7d512ae8a6a638ba6c27afca92df2e2d64e8095
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-12-07 20:07:31 +0800

    refactor: csv dataset

--- torchlake/common/datasets/csv.py
+++ torchlake/common/datasets/csv.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import pandas as pd
 from typing import Callable
 from torch.utils.data import Dataset
-from torchvision.transforms import ToTensor
 
 
 class CSVDataset(Dataset):
@@ -11,30 +10,31 @@ class CSVDataset(Dataset):
         self,
         path: str | Path,
         feature_columns: list[str],
-        target_column: str,
-        transform: Callable | None = ToTensor(),
-        label_transform: Callable | None = ToTensor(),
+        target_columns: list[str],
+        transform: Callable | None = None,
+        label_transform: Callable | None = None,
     ):
         self.path = path
         self.feature_columns = feature_columns
-        self.target_column = target_column
+        self.target_columns = target_columns
         self.transform = transform
         self.label_transform = label_transform
 
         self.load_data()
 
     def load_data(self):
-        self.data = pd.read_csv(
-            self.path,
-            usecols=[*self.feature_columns, *self.target_column],
-        )
+        kwargs = {}
+        if self.feature_columns or self.target_columns:
+            kwargs["usecols"] = self.feature_columns + self.target_columns
+
+        self.data = pd.read_csv(self.path, **kwargs)
 
     def __len__(self) -> int:
         return len(self.data)
 
     def __getitem__(self, idx: int) -> tuple:
-        features = self.data.iloc[idx, self.feature_columns].values
-        target = self.data.iloc[idx, self.target_column].values
+        features = self.data.iloc[idx][self.feature_columns].tolist()[0]
+        target = self.data.iloc[idx][self.target_columns].tolist()[0]
 
         if self.transform is not None:
             features = self.transform(features)
