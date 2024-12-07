21a4a92ddb56c01d1123249c855907ddfb1b3a73
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-12-07 19:50:44 +0800

    feat: csv dataset

--- torchlake/common/datasets/csv.py
+++ torchlake/common/datasets/csv.py
@@ -1,34 +1,45 @@
-import pandas as pd
-from torch.utils.data import Dataset
 from pathlib import Path
 
+import pandas as pd
+from typing import Callable
+from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor
 
-class CsvDataset(Dataset):
+
+class CSVDataset(Dataset):
     def __init__(
         self,
-        paths: list[str | Path],
+        path: str | Path,
         feature_columns: list[str],
         target_column: str,
+        transform: Callable | None = ToTensor(),
+        label_transform: Callable | None = ToTensor(),
     ):
-        self.paths = paths
+        self.path = path
         self.feature_columns = feature_columns
         self.target_column = target_column
-        self.data: list[pd.DataFrame] = []
-        self.count = 0
+        self.transform = transform
+        self.label_transform = label_transform
 
-        columns = [*self.feature_columns, *self.target_column]
-        self.data = []
-        for file_path in self.paths:
-            print(f"Reading from {file_path}...")
-            df = pd.read_csv(file_path, usecols=columns)
-            self.data.append(df)
-            self.count += len(df)
-        self.data = pd.concat(self.data, ignore_index=True)
+        self.load_data()
+
+    def load_data(self):
+        self.data = pd.read_csv(
+            self.path,
+            usecols=[*self.feature_columns, *self.target_column],
+        )
 
     def __len__(self) -> int:
-        return self.count
+        return len(self.data)
 
     def __getitem__(self, idx: int) -> tuple:
-        features = self.data.loc[idx, self.feature_columns].values
-        target = self.data.loc[idx, self.target_column].values
+        features = self.data.iloc[idx, self.feature_columns].values
+        target = self.data.iloc[idx, self.target_column].values
+
+        if self.transform is not None:
+            features = self.transform(features)
+
+        if self.label_transform is not None:
+            target = self.label_transform(target)
+
         return features, target
