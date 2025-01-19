ed8fe35c0c69616b674021aad488cfd052b3b571
gitE0Z9 <acrocanthosaurus627@gmail.com>
2025-01-19 18:26:20 +0800

    fix: hourglass loss to controller

---
+++ torchlake/pose_estimation/controller/trainer_hourglass.py
@@ -0,0 +1,28 @@
+from typing import Iterable
+
+import torch
+from torch import nn
+from torchlake.common.controller.trainer import RegressionTrainer
+from torchlake.common.utils.numerical import build_heatmap
+
+
+class StackedHourglassTrainer(RegressionTrainer):
+    def _calc_loss(
+        self,
+        y_hat: torch.Tensor,
+        row: tuple[Iterable],
+        criterion: nn.Module,
+    ) -> torch.Tensor:
+        x, y = row
+        y: torch.Tensor = y.to(self.device)
+
+        scales = torch.Tensor(
+            [s_x / s_yhat for s_x, s_yhat in zip(x.shape[2:], y_hat.shape[3:])]
+        ).to(self.device)
+        y /= scales[None, None, :]
+        # y_hat shape: batch, stack, channel, ...spatial
+        # TODO(won't do): multi instance shape: batch, instance, stack, channel
+        y = build_heatmap(y, spatial_shape=y_hat.shape[3:])
+        y = y.unsqueeze(1).expand_as(y_hat)
+
+        return criterion(y_hat, y.float())
--- torchlake/pose_estimation/models/hourglass/__init__.py
+++ torchlake/pose_estimation/models/hourglass/__init__.py
@@ -1,10 +1,8 @@
 from .model import StackedHourglass
 from .network import AuxiliaryHead, Hourglass2d
-from .loss import StackedHourglassLoss
 
 __all__ = [
     "StackedHourglass",
     "AuxiliaryHead",
     "Hourglass2d",
-    "StackedHourglassLoss",
 ]
