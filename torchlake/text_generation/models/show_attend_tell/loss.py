ed564a460cfff5bba242a702777c25c6a0da7eed
gitE0Z9 <acrocanthosaurus627@gmail.com>
2025-03-08 21:31:38 +0800

    fix: show attend tell loss

--- torchlake/text_generation/models/show_attend_tell/loss.py
+++ torchlake/text_generation/models/show_attend_tell/loss.py
@@ -5,9 +5,20 @@ from torch import nn
 
 class DoublyStochasticAttentionLoss(nn.Module):
 
-    def __init__(self, lambda_coef: float = 1e-5):
+    def __init__(
+        self,
+        lambda_coef: float = 1e-5,
+        ignore_index: int = -100,
+        reduction: str = "mean",
+        label_smoothing: float = 0,
+    ):
         super().__init__()
         self.lambda_coef = lambda_coef
+        self.cc = nn.CrossEntropyLoss(
+            ignore_index=ignore_index,
+            reduction=reduction,
+            label_smoothing=label_smoothing,
+        )
 
     def forward(
         self,
@@ -16,6 +27,6 @@ class DoublyStochasticAttentionLoss(nn.Module):
         att: torch.Tensor,
     ) -> torch.Tensor:
         att_sum = att.sum(1)
-        return F.cross_entropy(pred, gt) + self.lambda_coef * F.mse_loss(
+        return self.cc(pred, gt) + self.lambda_coef * F.mse_loss(
             att_sum, torch.ones_like(att_sum)
         )
