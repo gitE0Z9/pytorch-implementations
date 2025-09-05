98bc6cd162d9ae03d4b518143489f01fd55915d5
gitE0Z9 <acrocanthosaurus627@gmail.com>
2025-09-05 17:53:17 +0800

    refactor: fast style transfer test

--- torchlake/style_transfer/tests/test_fast_style_transfer.py
+++ torchlake/style_transfer/tests/test_fast_style_transfer.py
@@ -1,12 +1,11 @@
-import pytest
 import torch
 
 from ..models.fast_style_transfer import FastStyleTransfer
 from ..models.fast_style_transfer.network import ConvBlock, ResidualBlock
 
 
-class TestConvBlock:
-    def test_forward_shape(self):
+class TestNetwork:
+    def test_conv_block_forward_shape(self):
         x = torch.rand((1, 3, 32, 32))
 
         model = ConvBlock(3, 8, 3)
@@ -15,9 +14,7 @@ class TestConvBlock:
 
         assert y.shape == torch.Size((1, 8, 32, 32))
 
-
-class TestResidualBlock:
-    def test_forward_shape(self):
+    def test_residual_block_forward_shape(self):
         x = torch.rand((1, 3, 32, 32))
 
         model = ResidualBlock(3)
