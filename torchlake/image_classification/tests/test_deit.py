1d33e7e622b3f7197f79487bd3679ad3f62afa3f
gitE0Z9 <acrocanthosaurus627@gmail.com>
2025-10-24 16:35:31 +0800

    fix: deit test

--- torchlake/image_classification/tests/test_deit.py
+++ torchlake/image_classification/tests/test_deit.py
@@ -3,36 +3,40 @@ import torch
 from ..models.deit.loss import HardDistillation
 from ..models.deit.model import DeiT
 
+BATCH_SIZE = 2
+
 
 class TestModel:
     def test_forward_shape(self):
-        x = torch.rand((2, 3, 224, 224))
+        x = torch.rand((BATCH_SIZE, 3, 224, 224))
 
         model = DeiT(output_size=21)
 
         cls, dist = model(x)
 
-        assert cls.shape == torch.Size((2, 21))
-        assert dist.shape == torch.Size((2, 21))
+        assert cls.shape == torch.Size((BATCH_SIZE, 21))
+        assert dist.shape == torch.Size((BATCH_SIZE, 21))
 
 
 class TestLoss:
     def test_forward(self):
-        x = torch.rand(2, 10, requires_grad=True)
-        tx = torch.rand(2, 10)
-        y = torch.randint(10, (2,))
+        yhat = torch.rand(BATCH_SIZE, 10, requires_grad=True)
+        yhat_for_t = torch.rand(BATCH_SIZE, 10, requires_grad=True)
+        yhat_of_t = torch.rand(BATCH_SIZE, 10)
+        y = torch.randint(10, (BATCH_SIZE,))
 
         criterion = HardDistillation()
-        loss = criterion(x, tx, y)
+        loss = criterion(yhat, yhat_for_t, yhat_of_t, y)
 
         assert not torch.isnan(loss)
 
     def test_backward(self):
-        x = torch.rand(2, 10, requires_grad=True)
-        tx = torch.rand(2, 10)
-        y = torch.randint(10, (2,))
+        yhat = torch.rand(BATCH_SIZE, 10, requires_grad=True)
+        yhat_for_t = torch.rand(BATCH_SIZE, 10, requires_grad=True)
+        yhat_of_t = torch.rand(BATCH_SIZE, 10)
+        y = torch.randint(10, (BATCH_SIZE,))
 
         criterion = HardDistillation()
-        loss = criterion(x, tx, y)
+        loss = criterion(yhat, yhat_for_t, yhat_of_t, y)
 
         loss.backward()
