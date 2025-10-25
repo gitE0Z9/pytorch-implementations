da86f80ac32738570d00cfe42fcdfe6709a7e04d
gitE0Z9 <acrocanthosaurus627@gmail.com>
2025-10-26 00:15:24 +0800

    refactor: random func

--- torchlake/common/tests/test_utils_random.py
+++ torchlake/common/tests/test_utils_random.py
@@ -1,8 +1,14 @@
 import torch
-from torchlake.common.utils.random import generate_normal
+from torchlake.common.utils.random import generate_normal, generate_uniform
 
 
-def test_shape():
+def test_generate_normal_shape():
     x = generate_normal(1, 5)
+
+    assert x.shape == torch.Size((1, 5))
+
+
+def test_generate_uniform_shape():
+    x = generate_uniform(1, 5)
 
     assert x.shape == torch.Size((1, 5))
--- torchlake/common/utils/random.py
+++ torchlake/common/utils/random.py
@@ -7,5 +7,9 @@ def rand_color(maximum: int = 256, channel: int = 3) -> list[int]:
     return random.choices(range(maximum), k=channel)
 
 
-def generate_normal(*size: torch.Size, mu: float = 0, sd: float = 1) -> torch.Tensor:
-    return torch.empty(size).normal_(mu, sd)
+def generate_normal(*size: int, mu: float = 0, sd: float = 1) -> torch.Tensor:
+    return torch.randn(*size) * sd + mu
+
+
+def generate_uniform(*size: int, a: float = 0, b: float = 1) -> torch.Tensor:
+    return torch.rand(*size) * (b - a) + a
