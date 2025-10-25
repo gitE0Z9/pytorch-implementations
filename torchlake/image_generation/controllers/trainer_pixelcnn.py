2ae633a9d0be50242e5d874c04162281ce28846e
gitE0Z9 <acrocanthosaurus627@gmail.com>
2025-10-25 23:44:11 +0800

    refactor: conditional pixelcnn trainer

--- torchlake/image_generation/controllers/trainer_pixelcnn.py
+++ torchlake/image_generation/controllers/trainer_pixelcnn.py
@@ -2,6 +2,7 @@ from typing import Iterable
 
 import torch
 from torch import nn
+
 from torchlake.common.controller.trainer import ClassificationTrainer
 
 
@@ -13,6 +14,33 @@ class PixelCNNTrainer(ClassificationTrainer):
         criterion: nn.Module,
     ) -> torch.Tensor:
         _, y = row
+        y: torch.Tensor = y.to(self.device)
+
+        # B, C, 256, H, W => B*C, 256, H, W
+        return criterion(y_hat.view(-1, *y_hat.shape[2:]), y.long())
+
+
+class ConditionalPixelCNNTrainer(PixelCNNTrainer):
+    def _predict(
+        self,
+        row: tuple[Iterable],
+        model: nn.Module,
+        *args,
+        **kwargs,
+    ):
+        x, _, c = row
+        x = x.to(self.device)
+        c = c.to(self.device)
+
+        return model(x, cond=c, *args, **kwargs)
+
+    def _calc_loss(
+        self,
+        y_hat: torch.Tensor,
+        row: tuple[Iterable],
+        criterion: nn.Module,
+    ) -> torch.Tensor:
+        _, y, _ = row
         y: torch.Tensor = y.to(self.device)
 
         # B, C, 256, H, W => B*C, 256, H, W
