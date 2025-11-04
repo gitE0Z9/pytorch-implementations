d70f659e30acdd603a6d45c7875f737d69b355f8
gitE0Z9 <acrocanthosaurus627@gmail.com>
2025-11-04 23:03:49 +0800

    test: add lsgan tests

--- torchlake/image_generation/tests/test_lsgan.py
+++ torchlake/image_generation/tests/test_lsgan.py
@@ -1,9 +1,15 @@
 import torch
 
-from ..models.lsgan import LSGANDiscriminator, LSGANGenerator
+from ..models.lsgan import (
+    LSGANDiscriminator,
+    LSGANGenerator,
+    LSGANDiscriminatorLoss,
+    LSGANGeneratorLoss,
+)
 
 BATCH_SIZE = 1
 INPUT_CHANNEL = 3
+IMAGE_SIZE = 112
 LATENT_DIM = 16
 HIDDEN_DIM = 256
 
@@ -41,3 +47,55 @@ class TestModel:
         y = model(x)
 
         assert y.shape == torch.Size((BATCH_SIZE, 1))
+
+
+class TestLoss:
+    def test_lsgan_generator_loss_forward(self):
+        z = torch.rand((BATCH_SIZE, LATENT_DIM))
+
+        g = LSGANGenerator(
+            LATENT_DIM,
+            INPUT_CHANNEL,
+            HIDDEN_DIM,
+            num_block=2,
+            init_shape=(7, 7),
+        )
+        d = LSGANDiscriminator(
+            INPUT_CHANNEL,
+            64,
+            image_shape=(112, 112),
+            num_block=3,
+        )
+        xhat = g(z)
+        yhat_xhat = d(xhat)
+
+        criterion = LSGANGeneratorLoss()
+        loss = criterion(yhat_xhat)
+
+        assert not torch.isnan(loss)
+
+    def test_lsgan_discriminator_loss_forward(self):
+        z = torch.rand((BATCH_SIZE, LATENT_DIM))
+        x = torch.rand((BATCH_SIZE, INPUT_CHANNEL, IMAGE_SIZE, IMAGE_SIZE))
+
+        g = LSGANGenerator(
+            LATENT_DIM,
+            INPUT_CHANNEL,
+            HIDDEN_DIM,
+            num_block=2,
+            init_shape=(7, 7),
+        )
+        d = LSGANDiscriminator(
+            INPUT_CHANNEL,
+            64,
+            image_shape=(112, 112),
+            num_block=3,
+        )
+        xhat = g(z)
+        yhat_xhat = d(xhat)
+        yhat_x = d(x)
+
+        criterion = LSGANDiscriminatorLoss()
+        loss = criterion(yhat_xhat, yhat_x)
+
+        assert not torch.isnan(loss)
