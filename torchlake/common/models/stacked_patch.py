662c041d43ea4062614c08d5a97208175390605f
gitE0Z9 <acrocanthosaurus627@gmail.com>
2026-01-03 23:33:22 +0800

    doc

--- torchlake/common/models/stacked_patch.py
+++ torchlake/common/models/stacked_patch.py
@@ -17,12 +17,12 @@ class StackedPatch2d(nn.Module):
         _, c, h, w = x.shape
         patch_shape = (h // self.stride, w // self.stride)
         return (
-            # B, C * #patch, self.stride**2
+            # B, C * patch_size, #patch
             F.unfold(x, patch_shape, stride=patch_shape)
-            # B, C, #patch_v, #patch_h, self.stride**2
+            # B, C, patch_h, patch_w, #patch
             .unflatten(1, (c, *patch_shape))
-            # B, self.stride**2, C, #patch_v, #patch_h
+            # B, #patch, C, patch_h, patch_w
             .permute(0, 4, 1, 2, 3)
-            # B, self.stride**2 * C, #patch_v, #patch_h
+            # B, #patch * C, patch_h, patch_w
             .flatten(1, 2)
         )
