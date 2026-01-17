5269ca5884f773e342fb9b90528fb84233f59751
gitE0Z9 <acrocanthosaurus627@gmail.com>
2026-01-17 17:30:30 +0800

    refactor: pspnet

--- torchlake/semantic_segmentation/models/pspnet/model.py
+++ torchlake/semantic_segmentation/models/pspnet/model.py
@@ -1,66 +1,95 @@
-from typing import Literal
+from __future__ import annotations
 
 import torch
 import torch.nn.functional as F
-from annotated_types import T
 from torch import nn
-from torchlake.common.models import ResNetFeatureExtractor
 from torchvision.ops import Conv2dNormActivation
 
-from ...mixins.resnet_backbone import DeepLabStyleResNetBackboneMixin
+from torchlake.common.models.feature_extractor_base import ExtractorBase
+from torchlake.common.models.model_base import ModelBase
+
 from .network import PyramidPool2d
 
 
-class PSPNet(DeepLabStyleResNetBackboneMixin, nn.Module):
+class PSPNet(ModelBase):
 
     def __init__(
         self,
+        backbone: ExtractorBase,
         output_size: int = 1,
         bins_size: list[int] = [1, 2, 3, 6],
         dropout_prob: float = 0.5,
-        backbone_name: Literal["resnet50", "resnet101", "resnet152"] = "resnet50",
-        frozen_backbone: bool = False,
     ):
         """Pyramid spatial pooling network [1612.01105v2]
 
         Args:
+            backbone (ExtractorBase): feature extractor.
             output_size (int, optional): output size. Defaults to 1.
             bins_size (list[int], optional): size of pooled feature maps. Defaults to [1, 2, 3, 6].
             dropout_prob (float, optional): dropout probability. Defaults to 0.5.
-            backbone_name (Literal["resnet50", "resnet101", "resnet152"], optional): resnet network name. Defaults to "resnet50".
-            fronzen_backbone (bool, optional): froze the resnet backbone or not. Defaults to False.
         """
-        super().__init__()
-        hidden_dim = 2048
         self.dropout_prob = dropout_prob
-        self.output_size = output_size
-        self.backbone = self.build_backbone(backbone_name, frozen_backbone)
-        self.neck = PyramidPool2d(hidden_dim, bins_size)
-        self.head = nn.Sequential(
-            Conv2dNormActivation(hidden_dim * 2, hidden_dim // 4, 3),
-            nn.Dropout2d(dropout_prob),
-            nn.Conv2d(hidden_dim // 4, output_size, 1),
+        self.bins_size = bins_size
+        super().__init__(
+            backbone.input_channel,
+            output_size,
+            foot_kwargs={"backbone": backbone},
         )
 
-    def train(self: T, mode: bool = True) -> T:
+    def build_foot(self, _, **kwargs):
+        self.foot: ExtractorBase = kwargs.pop("backbone")
+
+    def build_neck(self, **kwargs):
+        self.neck = nn.Sequential(
+            PyramidPool2d(self.foot.hidden_dim_32x, self.bins_size),
+        )
+
+    def build_head(self, output_size, **kwargs):
+        self.head = nn.Sequential(
+            Conv2dNormActivation(
+                self.foot.hidden_dim_32x * 2,
+                self.foot.hidden_dim_32x // 4,
+                3,
+            ),
+            nn.Dropout2d(self.dropout_prob),
+            nn.Conv2d(
+                self.foot.hidden_dim_32x // 4,
+                output_size,
+                1,
+            ),
+        )
+
+    def train(self: PSPNet, mode: bool = True) -> PSPNet:
         result = super().train(mode)
 
         if not hasattr(self, "aux"):
-            hidden_dim = 1024
             self.aux = nn.Sequential(
-                Conv2dNormActivation(hidden_dim, hidden_dim // 4, 3),
+                Conv2dNormActivation(
+                    self.foot.hidden_dim_16x,
+                    self.foot.hidden_dim_16x // 4,
+                    3,
+                ),
                 nn.Dropout2d(p=self.dropout_prob),
-                nn.Conv2d(hidden_dim // 4, self.output_size, 1),
+                nn.Conv2d(
+                    self.foot.hidden_dim_16x // 4,
+                    self.output_size,
+                    1,
+                ),
             )
+
+        self.foot.fix_target_layers(("3_1", "4_1"))
+
+        return result
+
+    def eval(self: PSPNet) -> PSPNet:
+        result = super().eval()
+
+        self.foot.fix_target_layers(("4_1",))
 
         return result
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # extract features
-        feature_names = ["4_1"]
-        if self.training:
-            feature_names.append("3_1")
-        features: list[torch.Tensor] = self.backbone(x, feature_names)
+        features: list[torch.Tensor] = self.foot(x)
 
         if self.training:
             aux, y = features
--- torchlake/semantic_segmentation/tests/test_pspnet.py
+++ torchlake/semantic_segmentation/tests/test_pspnet.py
@@ -1,61 +1,80 @@
+import pytest
 import torch
 
-from ..models.pspnet import PSPNet, PSPLoss, PyramidPool2d
+from torchlake.semantic_segmentation.models.deeplabv2.network import (
+    deeplab_v2_style_resnet,
+)
+
+from ..models.pspnet import PSPLoss, PSPNet, PyramidPool2d
+
+BATCH_SIZE = 2
+INPUT_CHANNEL = 3
+HIDDEN_DIM = 8
+IMAGE_SIZE = 224
+FEATURE_MAP_SIZE = IMAGE_SIZE // 32
+NUM_CLASS = 21
 
 
-class TestPSPNet:
-    def test_training_forward_shape(self):
-        x = torch.rand((16, 3, 224, 224))
+class TestNetwork:
+    def test_pyramid_pool_2d_forward_shape(self):
+        x = torch.rand((BATCH_SIZE, HIDDEN_DIM, FEATURE_MAP_SIZE, FEATURE_MAP_SIZE))
 
-        model = PSPNet(21)
-        model.train()
-
-        y, aux = model(x)
-
-        assert y.shape == torch.Size((16, 21, 224, 224))
-        assert aux.shape == torch.Size((16, 21, 224, 224))
-
-    def test_eval_forward_shape(self):
-        x = torch.rand((16, 3, 224, 224))
-
-        model = PSPNet(21).eval()
-
-        y = model(x)
-
-        assert y.shape == torch.Size((16, 21, 224, 224))
-
-
-class TestPyramidPool2d:
-    def test_forward_shape(self):
-        x = torch.rand((16, 2048, 7, 7))
-
-        model = PyramidPool2d(2048)
+        model = PyramidPool2d(HIDDEN_DIM)
 
         y = model(x)
 
-        assert y.shape == torch.Size((16, 2048 * 2, 7, 7))
+        assert y.shape == torch.Size(
+            (BATCH_SIZE, HIDDEN_DIM * 2, FEATURE_MAP_SIZE, FEATURE_MAP_SIZE)
+        )
 
 
-class TestPSPLoss:
-    def test_forward(self):
-        pred = torch.rand((16, 21, 224, 224))
-        aux = torch.rand((16, 21, 224, 224))
-        target = torch.randint(0, 21, (16, 224, 224))
+class TestModel:
+    @pytest.mark.parametrize("is_training", ((True,), (False,)))
+    def test_pspnet_forward_shape(self, is_training: bool):
+        x = torch.rand((BATCH_SIZE, INPUT_CHANNEL, IMAGE_SIZE, IMAGE_SIZE))
 
-        model = PSPLoss()
+        backbone = deeplab_v2_style_resnet("resnet50", trainable=False)
+        model = PSPNet(backbone, NUM_CLASS)
 
-        loss = model(pred, aux, target)
+        if is_training:
+            model.train()
+            y, aux = model(x)
+            assert y.shape == torch.Size(
+                (BATCH_SIZE, NUM_CLASS, IMAGE_SIZE, IMAGE_SIZE)
+            )
+            assert aux.shape == torch.Size(
+                (BATCH_SIZE, NUM_CLASS, IMAGE_SIZE, IMAGE_SIZE)
+            )
+        else:
+            model.eval()
+            y = model(x)
+            assert y.shape == torch.Size(
+                (BATCH_SIZE, NUM_CLASS, IMAGE_SIZE, IMAGE_SIZE)
+            )
+
+
+class TestLoss:
+    def test_psp_loss_forward(self):
+        pred = torch.rand((BATCH_SIZE, NUM_CLASS, IMAGE_SIZE, IMAGE_SIZE))
+        aux = torch.rand((BATCH_SIZE, NUM_CLASS, IMAGE_SIZE, IMAGE_SIZE))
+        target = torch.randint(0, NUM_CLASS, (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE))
+
+        criterion = PSPLoss()
+
+        loss = criterion(pred, aux, target)
 
         assert not torch.isnan(loss)
 
-    def test_backward(self):
-        x = torch.rand((16, 3, 224, 224))
-        target = torch.randint(0, 21, (16, 224, 224))
-        model = PSPNet(21).eval()
+    def test_psp_loss_backward(self):
+        x = torch.rand((BATCH_SIZE, INPUT_CHANNEL, IMAGE_SIZE, IMAGE_SIZE))
+        target = torch.randint(0, NUM_CLASS, (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE))
+
+        backbone = deeplab_v2_style_resnet("resnet50", trainable=False)
+        model = PSPNet(backbone, NUM_CLASS)
         model.train()
 
         y = model(x)
-        model = PSPLoss()
 
-        loss = model(*y, target)
+        criterion = PSPLoss()
+        loss = criterion(*y, target)
         loss.backward()
