a9b06d46e762d2d5a6ad8ff1c6a0298fc8badd2f
gitE0Z9 <acrocanthosaurus627@gmail.com>
2026-04-08 21:33:32 +0800

    vdsr, initialization and format

--- torchlake/super_resolution/models/vdsr/model.py
+++ torchlake/super_resolution/models/vdsr/model.py
@@ -2,34 +2,38 @@ from torch import nn
 from torchlake.common.models.model_base import ModelBase
 from torchlake.common.models.residual import ResBlock
 
+
 class VDSR(ModelBase):
-    def __init__(self, input_channel: int = 1, hidden_dim: int = 64, num_blocks: int = 20):
+    def __init__(
+        self,
+        input_channel: int = 1,
+        hidden_dim: int = 64,
+        num_blocks: int = 20,
+    ):
         self.num_blocks = num_blocks
         self.hidden_dim = hidden_dim
         super().__init__(input_channel, input_channel)
-        
+
         for _, m in self.named_modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight)
-        
-        
+                nn.init.kaiming_normal_(m.weight, nonlinearity="relu")
+                nn.init.zeros_(m.bias)
+
     def build_foot(self, input_channel, **kwargs):
         blocks = nn.Sequential(
-            nn.Conv2d(input_channel, self.hidden_dim, 3, padding=1),
-            nn.ReLU(True)
+            nn.Conv2d(input_channel, self.hidden_dim, 3, padding=1), nn.ReLU(True)
         )
-        for _ in range(self.num_blocks-2):
+        for _ in range(self.num_blocks - 2):
             blocks.append(nn.Conv2d(self.hidden_dim, self.hidden_dim, 3, padding=1))
             blocks.append(nn.ReLU(True))
         blocks.append(nn.Conv2d(self.hidden_dim, input_channel, 3, padding=1))
-            
+
         self.foot = ResBlock(
             input_channel,
             input_channel,
             block=blocks,
             activation=None,
         )
-        
+
     def build_head(self, _, **kwargs):
         self.head = nn.Identity()
-
