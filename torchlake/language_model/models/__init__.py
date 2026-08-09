246e2808b2450e036086d4ef9eb0d5b1859ae66d
gitE0Z9 <acrocanthosaurus627@gmail.com>
2026-08-09 17:35:36 +0800

    fix: unreleased bug

--- torchlake/language_model/models/__init__.py
+++ torchlake/language_model/models/__init__.py
@@ -1,18 +1,19 @@
-from .character_aware.model import (
-    CharaceterAwareLM,
-    characeter_aware_lm_large,
-    characeter_aware_lm_small,
-)
+# from .character_aware.model import (
+#     CharaceterAwareLM,
+#     characeter_aware_lm_large,
+#     characeter_aware_lm_small,
+# )
 from .glove.model import GloVe
 from .vlbl.model import IVLBL, VLBL
-from .subword.model import SubwordLM
+
+# from .subword.model import SubwordLM
 
 __all__ = [
     "VLBL",
     "IVLBL",
     "GloVe",
-    "CharaceterAwareLM",
-    "characeter_aware_lm_small",
-    "characeter_aware_lm_large",
-    "SubwordLM",
+    # "CharaceterAwareLM",
+    # "characeter_aware_lm_small",
+    # "characeter_aware_lm_large",
+    # "SubwordLM",
 ]
