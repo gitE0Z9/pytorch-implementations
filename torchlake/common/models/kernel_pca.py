feat (kernel pca): fix over normalization of kernel & fix transform method &

torchlake/common/models/kernel_pca.py
@@ -25,50 +25,84 @@ def rbf_kernel(x: torch.Tensor, gamma: float = 1.0) -> torch.Tensor:
 def hellinger_kernel(x: torch.Tensor, is_normalized: bool = True) -> torch.Tensor:
     if not is_normalized:
         x = F.normalize(x, 1, 1)
-    x = x.sqrt()
-    return torch.cdist(x, x, p=2) / sqrt(2)
+
+    return torch.cdist(x.sqrt(), x, p=2) / sqrt(2)
+
+
+def linear_kernel_transform(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    return x @ y
+
+
+def rbf_kernel_transform(
+    x: torch.Tensor,
+    y: torch.Tensor,
+    gamma: float = 1.0,
+) -> torch.Tensor:
+    z = torch.cdist(x, y, p=2)
+    return torch.exp(-gamma * z**2)
+
+
+def hellinger_kernel_transform(
+    x: torch.Tensor,
+    y: torch.Tensor,
+    is_normalized: bool = True,
+) -> torch.Tensor:
+    if not is_normalized:
+        x = F.normalize(x, 1, 1)
+
+    return torch.cdist(x.sqrt(), y, p=2) / sqrt(2)
 
 
 def center_kernel(K: torch.Tensor) -> torch.Tensor:
-    N = K.shape[0]
-    row_mean = K.mean(1, keepdim=True) / N
-    col_mean = K.mean(0, keepdim=True) / N
+    row_mean = K.mean(1, keepdim=True)
+    col_mean = K.mean(0, keepdim=True)
     total_mean = row_mean.mean()
     K_centered = K - row_mean - col_mean + total_mean
     return K_centered
+
+
+KernelFuncPair = tuple[
+    Callable[[torch.Tensor], torch.Tensor],  # fit
+    Callable[[torch.Tensor, torch.Tensor], torch.Tensor],  # transform
+]
 
 
 class KernelPCA(nn.Module):
     def __init__(
         self,
         n_components: int,
-        kernel: str | KernelEnum | Callable[[torch.Tensor], torch.Tensor],
+        kernel: str | KernelEnum | KernelFuncPair,
         kernel_params: dict = {},
     ):
-        super(KernelPCA, self).__init__()
+        super().__init__()
         self.n_components = n_components
-        self.kernel = (
-            kernel
-            if isinstance(kernel, Callable)
-            else self.kernel_mapping[KernelEnum(kernel)]
-        )
+
+        if isinstance(kernel, str) | isinstance(kernel, KernelEnum):
+            self.kernel, self.kernel_transform = self.kernel_mapping[KernelEnum(kernel)]
+        else:
+            assert (
+                len(kernel) == 2
+            ), "should provide the kernel function for fit and transform"
+            self.kernel, self.kernel_transform = kernel
+
         self.kernel_params = kernel_params
 
     @property
     def kernel_mapping(self):
         return {
-            KernelEnum.LINEAR: linear_kernel,
-            KernelEnum.RBF: rbf_kernel,
-            KernelEnum.HELLINGER: hellinger_kernel,
+            KernelEnum.LINEAR: (linear_kernel, linear_kernel_transform),
+            KernelEnum.RBF: (rbf_kernel, rbf_kernel_transform),
+            KernelEnum.HELLINGER: (hellinger_kernel, hellinger_kernel_transform),
         }
 
     def fit(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        self.x_fit = x
-
+        print("1. kernel computation")
         K = self.kernel(x, **self.kernel_params)
 
+        print("2. centering")
         K_centered = center_kernel(K)
 
+        print("3. svd")
         # eigvals, eigvecs = torch.linalg.eigh(K_centered)
         eigvecs, eigvals, _ = torch.linalg.svd(K_centered)
 
@@ -76,9 +110,10 @@ class KernelPCA(nn.Module):
 
         self.eigenvectors: torch.Tensor = eigvecs[:, : self.n_components]
         self.eigenvalues: torch.Tensor = eigvals[: self.n_components]
+        self.x_fit = x
 
     def transform(self, x: torch.Tensor) -> torch.Tensor:
-        K_new: torch.Tensor = self.kernel(x, self.x_fit, **self.kernel_params)
+        K_new: torch.Tensor = self.kernel_transform(x, self.x_fit, **self.kernel_params)
 
         K_new_centered = (
             K_new

torchlake/common/tests/models/test_kernel_pca.py
@@ -4,6 +4,11 @@ import pytest
 from ...models import KernelPCA
 from ...models.kernel_pca import KernelEnum
 
+BATCH_SIZE = 8
+VOCAB_SIZE = 100
+SEQ_LEN = 10
+LATENT_DIM = 2
+
 
 class TestKernelPCA:
     @pytest.mark.parametrize(
@@ -24,16 +29,16 @@ class TestKernelPCA:
         ],
     )
     def test_output_shape(self, name: str, kernel: str):
-        x = torch.randn(8, 10)
+        x = torch.randn(BATCH_SIZE, SEQ_LEN)
         kernel_params = {}
 
         if kernel == KernelEnum.HELLINGER:
-            x = torch.randint(0, 100, (8, 10)).float()
+            x = torch.randint(0, VOCAB_SIZE, (BATCH_SIZE, SEQ_LEN)).float()
             kernel_params["is_normalized"] = False
 
-        model = KernelPCA(2, kernel, kernel_params)
+        model = KernelPCA(LATENT_DIM, kernel, kernel_params)
 
         model.fit(x)
 
-        assert model.eigenvalues.shape == torch.Size((2,))
-        assert model.eigenvectors.shape == torch.Size((8, 2))
+        assert model.eigenvalues.shape == torch.Size((LATENT_DIM,))
+        assert model.eigenvectors.shape == torch.Size((BATCH_SIZE, LATENT_DIM))

torchlake/representation/models/hellinger/model.py
@@ -4,10 +4,10 @@ import joblib
 import torch
 import torch.nn.functional as F
 from torch import nn
+
+from torchlake.common.helpers.counter import CooccurrenceCounter
 from torchlake.common.models import KernelPCA
 from torchlake.common.models.kernel_pca import KernelEnum
-
-from .helper import CooccurrenceCounter
 
 
 class HellingerPCA(nn.Module):
@@ -29,25 +29,12 @@ class HellingerPCA(nn.Module):
     def embedding(self) -> torch.Tensor:
         return self.model.eigenvectors
 
-    def fit(self, co_occurrence: CooccurrenceCounter, vocab_counts: torch.LongTensor):
-        # build co-occurrence matrix
-        count_source = co_occurrence.get_pair_counts().items()
-        row_indices = []
-        col_indices = []
-        values = []
-        for (gram, context), count in count_source:
-            row_indices.append(gram)
-            col_indices.append(context)
-            values.append(count)
-        counts = torch.sparse_coo_tensor(
-            [row_indices, col_indices],
-            values,
-            size=(self.vocab_size, self.vocab_size),
-        ).to(vocab_counts.device)
+    def fit(self, counter: CooccurrenceCounter, vocab_counts: torch.LongTensor):
+        counts = counter.get_tensor().coalesce().to(vocab_counts.device)
 
-        # select most significant context words
         most_significant_context = vocab_counts.topk(self.maximum_context_size).indices
         counts: torch.Tensor = counts.index_select(1, most_significant_context)
+        # vocab_size, maximum_context_size
         prob = counts.to_dense().float()
         self.model.fit(F.normalize(prob, 1, 1))
 